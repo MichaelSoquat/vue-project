84fa150fdb479225ee38bc0d44fbdb01000c57c2
Soquat <msoquat@questax.com>
2023-01-20 15:54:32 +0100

    added tasks

---
+++ vue-project/src/App.vue
@@ -0,0 +1,102 @@
+
+
+<template>
+  <div class="container">
+    <Header title="Task Tracker"></Header>
+    <Tasks @delete-task="deleteTask" :tasks="tasks"></Tasks>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    deleteTask(id) {
+      console.log("delete", id)
+      if(confirm("Are you sure?")){
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+  },
+
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "Feb 5th at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "Feb 6th at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Food Shopping",
+        day: "Feb 5th at 2:30pm",
+        reminder: false,
+      },
+    ];
+  },
+};
+</script>
+
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ vue-project/src/components/Tasks.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="task in tasks" :key="task.id">
+      <Task @delete-task="$emit('delete-task', task.id)" :task="task"></Task>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Task from "./Task.vue";
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+    components: {
+        Task,
+    },
+    emits: ['delete-task'],
+};
+</script>
